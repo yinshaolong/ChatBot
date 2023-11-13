f2e6084dc041c9257674d6760a3ac3da536f64c0
omgitsjohn <syin18@my.bcit.ca>
2023-11-12 21:42:35 -0800

    function_call attribute was causing error. taking response and manually inputting it into assistant content instead

--- chatbot.py
+++ chatbot.py
@@ -12,28 +12,29 @@ def get_reply(model):
     model=model,
     messages=conversation,
     max_tokens = 20
-    )
-# .choices[0].message
+    ).choices[0].message.content
 
 def chatbot(messages:list)->None:
     while True:
         try:
             conversation.append( {'role': 'user', 'content':input("Enter your message: ")})
-            response = get_reply(models[1]).to_dict()
+            response = get_reply(models[1])
             #appends gpt assistant response to conversation
-            conversation.append(response)
+            conversation.append({'role': 'assistant', 'content':response})
 
             print(conversation)
             print("response", response)    
-            print(response.content)    
+            print(response)    
 
             
         except KeyboardInterrupt:
             print("Exiting....")
             break
 
-# chatbot(conversation)
-conversation.append( {'role': 'user', 'content':input("Enter your message: ")})
-#__dict__ allows for dict conversion
-    #cant use to_dict()
-print(get_reply(models[1]).choices[0].message.__dict__)
+chatbot(conversation)
+
+
+# conversation.append( {'role': 'user', 'content':input("Enter your message: ")})
+# #__dict__ allows for dict conversion
+#     #cant use to_dict()
+# print(get_reply(models[1]).choices[0].message.__dict__)
