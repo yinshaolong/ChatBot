f47f2d758b87e4d4faf1ec70f3f94f1aef2e4064
omgitsjohn <syin18@my.bcit.ca>
2023-11-12 21:21:10 -0800

    removed content from choices[0].message return in get_reply to get gpts response

--- chatbot.py
+++ chatbot.py
@@ -2,25 +2,28 @@ from openai import OpenAI
 from dotenv import dotenv_values
 
 config = dotenv_values(".env")['OPENAI_KEY']
-
 client = OpenAI(api_key = config)
-
 models = {1:"gpt-3.5-turbo", 2:"gpt-4-1106-preview",}
+
+conversation = []
 
 def get_reply(model):
     return client.chat.completions.create(
     model=model,
-    messages=[ {'role': 'user', 'content':input("Enter your message: ")}],
+    messages=conversation,
     max_tokens = 20
-    ).choices[0].message.content
+    ).choices[0].message
 
-print(get_reply(models[1]))
+def chatbot(messages:list)->None:
+    while True:
+        try:
+            conversation.append( {'role': 'user', 'content':input("Enter your message: ")})
+            response = get_reply(models[1])
+            print("response", response)    
+            
+            print(conversation)
+        except KeyboardInterrupt:
+            print("Exiting....")
+            break
 
-while True:
-    try:
-        res = get_reply(models[1])
-        print(res)    
-    except KeyboardInterrupt:
-        print("Exiting....")
-        break
-print(res)
+chatbot(conversation)
